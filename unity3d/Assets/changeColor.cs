d31a07300387cb283b11632ff48a66b93dd672e5
ikoojoshi <ipshitamjoshi@gmail.com>
2020-10-03 12:36:00 +0530

    unity project added

---
+++ unity3d/Assets/changeColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void Black()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+}
