740e6dd20d0194cea03811be9c430ad89d7206ae
ikoojoshi <ipshitamjoshi@gmail.com>
2020-10-03 17:31:39 +0530

    added solar scene

---
+++ unity3d/Assets/Scripts/lookat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookat : MonoBehaviour
+{
+
+    static public GameObject target; 
+
+    void Start()
+    {
+        if (target == null)
+        {
+            target = this.gameObject;
+            Debug.Log("LookAtTarget target not specified. Defaulting to parent GameObject 333");
+        }
+    }
+    
+    void Update()
+    {
+        if (target)
+            transform.LookAt(target.transform);
+    }
+}
---
+++ unity3d/Assets/Scripts/move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+
+    public GameObject target; // the target that the camera should look at
+
+    void Start()
+    {
+        if (target == null)
+        {
+            target = this.gameObject;
+            Debug.Log("ChangeLookAtTarget target not specified. Defaulting to parent GameObject 1111");
+        }
+    }
+    
+    void OnMouseDown()
+    {
+        lookat.target = target;
+        Camera.main.fieldOfView = 60 * target.transform.localScale.x;
+        //Camera.main.fieldOfView = Mathf.Clamp(60 * target.transform.localScale.x, 1, 200);
+    }
+}
