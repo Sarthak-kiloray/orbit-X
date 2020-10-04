fa961d3895cada394ccef8cf8203913a2ce9da86
ikoojoshi <ipshitamjoshi@gmail.com>
2020-10-04 07:06:53 +0530

    flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template, render_template_string
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/satellite')
+def satellite():
+    return render_template("satellite.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
