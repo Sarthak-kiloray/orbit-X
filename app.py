29d251fb0255272a4d500417271e571b25ff9909
ikoojoshi <ipshitamjoshi@gmail.com>
2020-10-04 08:31:23 +0530

    modified starlink

--- app.py
+++ app.py
@@ -12,13 +12,20 @@ def index():
 def satellite():
     return render_template("satellite.html")
 
+@app.route('/satellites')
+def satellites():
+    return render_template("satellites.html")
+
+
 @app.route('/strela')
 def strela():
     return render_template("strela.html")
 
-@app.route('/satellites')
-def satellites():
-    return render_template("satellites.html")
+
+@app.route('/starlink')
+def starlink():
+    return render_template("starlink2.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
