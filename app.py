ceb2c46b806579697cef02c1ce6a30c27e1fa8bb
ikoojoshi <ipshitamjoshi@gmail.com>
2020-10-04 13:30:56 +0530

    satellites updated

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, render_template_string
 
 import pandas as pd
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,7 +16,13 @@ def satellite():
 
 @app.route('/satellites')
 def satellites():
-    return render_template("satellites.html")
+    df = pd.read_excel('data-preprocessing/UCS-Satellite-Database-4-1-2020.xls')
+    col1 = df['Name of Satellite, Alternate Names'].tolist()
+    col2 = df['Country of Operator/Owner'].tolist()
+    col3 = df['Operator/Owner'].tolist()
+    col4 = df['Users'].tolist()
+    col5 = df['Purpose'].tolist()
+    return render_template("satellites.html", col1=col1, col2=col2, col3=col3, col4=col4,  col5=col5, len1=len(col1))
 
 
 @app.route('/strela')
